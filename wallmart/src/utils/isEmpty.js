da4f32ac8d38e1d4746e928e18b93606e0eef538
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-06-27 11:38:15 +0545

    removed frontend app

--- wallmart/src/utils/isEmpty.js
+++
@@ -1,4 +0,0 @@
-export const isEmpty = value => value === undefined || value === null ||
-                                (typeof value === 'object' && Object.keys(value).length === 0) ||
-                                (typeof value === 'string' && value.trim().length === 0);
-                        
